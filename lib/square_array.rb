3687678d0abf7d18ec5e2178e5b8f5325bbd82d3
Nadine Edwards <edwardsnadine1985@gmail.com>
2020-09-30 23:41:40 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   while array.length do
     new_array=[]
     #array=array.length*2 
-    new_array.push(array.length]*2)
+    new_array.push(array.length*2)
     array+=1
     
   end  
